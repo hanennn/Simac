7cdc884be6766d80f20dada8415ac413f45bf1ef
hanennn <hanen.bennaceur@esprit.tn>
2026-08-03 23:00:37 +0100

    code OTP crypté

--- src/main/java/org/example/simac/service/OtpService.java
+++ src/main/java/org/example/simac/service/OtpService.java
@@ -3,10 +3,10 @@ package org.example.simac.service;
 import lombok.RequiredArgsConstructor;
 import org.example.simac.entity.CodeOtp;
 import org.example.simac.repository.CodeOtpRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 import java.util.Random;
 
 @Service
@@ -15,32 +15,32 @@ public class OtpService {
 
     private final CodeOtpRepository codeOtpRepository;
     private final NotificationEmailService notificationEmailService;
+    private final PasswordEncoder passwordEncoder;
 
     public void genererEtEnvoyerOtp(String email) {
         String code = String.format("%06d", new Random().nextInt(999999));
+
         CodeOtp otp = new CodeOtp();
         otp.setEmail(email);
-        otp.setCode(code);
+        otp.setCode(passwordEncoder.encode(code)); // stock hash
         otp.setDateExpiration(LocalDateTime.now().plusMinutes(5));
         otp.setUtilise(false);
         codeOtpRepository.save(otp);
         System.out.println("code "+code);
+        //envoi code
         notificationEmailService.envoyerEmailOtp(email, code);
     }
 
     public boolean verifierOtp(String email, String codeSaisi) {
         var otp = codeOtpRepository
                 .findTopByEmailAndUtiliseFalseOrderByDateExpirationDesc(email)
-                .orElseThrow(()-> new IllegalArgumentException("Le code de connexion est invalide"));
-        if (otp==null) {
-            return false;
-        }
+                .orElseThrow(() -> new IllegalArgumentException("Le code de connexion est invalide"));
 
         if (otp.getDateExpiration().isBefore(LocalDateTime.now())) {
             return false; // code expire
         }
 
-        if (!otp.getCode().equals(codeSaisi)) {
+        if (!passwordEncoder.matches(codeSaisi, otp.getCode())) {
             return false; // code incorrect
         }
 
